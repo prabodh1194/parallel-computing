0936596e267c860470ad44e980d10a8bcc025abe
Prabodh Agarwal <prabodh1194@gmail.com>
2016-02-10 15:18:15 +0530

    Partial fix

--- ass1/file.c
+++ ass1/file.c
@@ -14,12 +14,14 @@
 #define FILE_NAME_SIZE 256 //number of permissible characters in file name in linux
 #define FILE_PATH_SIZE 4096 //number of permissible characters in file name in linux
 
-void getFiles(char *path, char ***buff, int offset);
+void getFiles(char *path, char ***buff, int *offset);
+void printContent(char **buff, int size);
+void compare2(char **buff1, char **buff2, int size1, int size2);
 
 int main(int argc, char * const *argv)
 {
     char *dir1, *dir2, ***buff;
-    int j,i,fileno = FILE_NUMBER, c;
+    int j,i,fileno = FILE_NUMBER, c, size1, size2;
 
     dir1 = argv[1];
     dir2 = argv[2];
@@ -59,17 +61,19 @@ int main(int argc, char * const *argv)
     }
 
     printf("\n\nFiles in %s\n",dir1);
-    getFiles(dir1, buff, 0);
+    getFiles(dir1, buff, &size1);
+    printContent(*buff, size1);
 
-    printf("\n\nFiles in %s\n",dir1);
-    getFiles(dir2, buff+1, 0);
+    printf("\n\nFiles in %s\n",dir2);
+    getFiles(dir2, buff+1, &size2);
+    printContent(buff[1], size2);
 
     //printf("Files %d\nDir1:%s\nDir2:%s\n",fileno,dir1,dir2);
     return 0;
 }
 
 /* Used http://www.thegeekstuff.com/2012/06/c-directory/ to write this function*/
-void getFiles(char *path, char ***buff, int offset)
+void getFiles(char *path, char ***buff, int *offset)
 {
     char name[FILE_PATH_SIZE];
     DIR *dp = NULL;
@@ -89,12 +93,27 @@ void getFiles(char *path, char ***buff, int offset)
             else
                 sprintf(name,"%s%s",path,dptr->d_name);
 
-            printf("%s\n",name);
+            //printf("%s\n",name);
 
             if(dptr->d_type == DT_DIR && strcmp(dptr->d_name,".")!=0 && strcmp(dptr->d_name,"..")!=0)
                 getFiles(name, buff, offset);
             else if(strcmp(dptr->d_name,".")!=0 && strcmp(dptr->d_name,"..")!=0)
-                strcpy(*(*buff+offset++),name);
+                strcpy(*(*buff+(*offset)++),name);
         }
     }
 }
+
+void printContent(char **buff, int size)
+{
+    int i;
+    for(i=0;i<size;i++)
+    {
+        printf("%s\n",*buff);
+        buff++;
+    }
+}
+
+void compare2(char **buff1, char **buff2, int size1, int size2)
+{
+    int i,j;
+
